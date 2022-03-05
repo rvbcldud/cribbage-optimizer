651e27fccf84667427aa31c08e853b3607e9036a
rvbcldud <ryanlee6767@gmail.com>
2022-03-05 10:40:04 -0800

    Formatted and removed print statements in python classes

--- Card.py
+++ Card.py
@@ -1,9 +1,21 @@
 class Card:
-    KEYS = {1: 'Ace', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five',
-         6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', 11: 'Jack',
-         12: 'Queen', 13: 'King'}
+    KEYS = {
+        1: "Ace",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+        10: "Ten",
+        11: "Jack",
+        12: "Queen",
+        13: "King",
+    }
 
-    SUITS = {0: 'Hearts', 1: 'Diamonds', 2: 'Clubs', 3: 'Spades'}
+    SUITS = {0: "Hearts", 1: "Diamonds", 2: "Clubs", 3: "Spades"}
 
     def __init__(self, key, suit):
         self.key = key
@@ -14,8 +26,8 @@ class Card:
             self.value = 10
 
     def read_card(self):
-        key = self.KEYS.get(self.key) # 0 -> Ace
-        suit = self.SUITS.get(self.suit) # 1 -> Diamonds
-        card_name = key + ' of ' + suit
+        key = self.KEYS.get(self.key)  # 1 -> Ace
+        suit = self.SUITS.get(self.suit)  # 1 -> Diamonds
+        card_name = key + " of " + suit
 
         return card_name
--- Hand.py
+++ Hand.py
@@ -1,8 +1,9 @@
 from itertools import combinations
+
 import numpy as np
 
-class Hand:
 
+class Hand:
     def __init__(self):
         self.cards = []
 
@@ -16,30 +17,24 @@ class Hand:
 
     def display_hand(self):
         for i in self.cards:
-            print('\t' + i.read_card())
+            print("\t" + i.read_card())
 
     def calculate_hand(self, cut_card=None):
-        '''
+        """
         Calculate all 5, 4, 3, 2 combinations
         This means we need to split up all kinds of earning points
         - Pairs: Check if there is more than one of each card in hand
-        - Runs: Look at every 3, 4 card combination
+        - Runs: Look at every 3, 4, and 5 card combination
           - If one 5, no 4s
           - If one or two 4, no 3s
             - Two 4 when they don't equal each other
-        - 15s: If 5 long 15, no 4s, etc.  
+        - 15s: If 5 long 15, no 4s, etc.
           - Can only be 1 or 2 4s, but can also have 4s or 2s
           - To check for duplicates, compare sorted lists
-          - If calue of card >= 10, real value is 10
-          - Create new variable value and change what is currently value to key
-            - Set this when you populate the deck
-        '''
+        """
 
         if cut_card is not None:
             hand = self.add_card(cut_card)
-            print('CUT CARD DETECTED')
-
-        # TODO incorporate cut card!
 
         def populate_combos(self):
             self.display_hand()
@@ -53,7 +48,6 @@ class Hand:
         combos = populate_combos(hand)
 
         def calculate_pairs(self):
-            print('CALCULATING PAIRS')
             # Create a list of cards, but only the keys
             key_list = []
             for card in self.cards:
@@ -82,18 +76,15 @@ class Hand:
                 elif len(i) == 4:
                     points += 8
 
-
-            print(final_list)
-
             return points
 
         def calculate_runs(combo_list):
-            print('CALCULATING RUNS')
-            run_list = []
             def check_consecutive(lst):
                 # Checks to see if the list of integers is consecutive
                 n = len(lst) - 1
-                return(sum(np.diff(lst) == 1) >= n)
+                return sum(np.diff(lst) == 1) >= n
+
+            run_list = []
 
             for hand in combo_list:
                 key_list = []
@@ -105,13 +96,14 @@ class Hand:
                         max_len = 4
                     elif len(key_list) == 5:
                         max_len = 5
-                    print(key_list)
                     run_list.append(hand)
-                    print('cards :')
-                    for card in hand:
-                        print(card.read_card())
 
-            run_list=[x for x in run_list if len(x)==max_len]
+            # Filter the list of runs for the only possible combinations
+            # Rules:
+            # - Runs: Look at every 3, 4, and 5 card combination
+            #   - If one 5, no 4s
+            #   - If one or two 4, no 3s
+            run_list = [x for x in run_list if len(x) == max_len]
 
             points = 0
 
@@ -125,9 +117,7 @@ class Hand:
 
             return points
 
-
         def calculate_fifteen(combo_list):
-            print('CALCULATING 15s')
             fifteen_list = []
             for hand in combo_list:
                 temp = []
@@ -136,8 +126,6 @@ class Hand:
                 # Checks if the pair is equal to 15. If so, add to list of 15s
                 if sum(temp) == 15:
                     fifteen_list.append(temp)
-                    
-            print(fifteen_list)
 
             points = 0
 
@@ -146,21 +134,13 @@ class Hand:
 
             return points
 
-
-
         pairs = calculate_pairs(self)
         runs = calculate_runs(combos)
         fifteens = calculate_fifteen(combos)
 
         total_points = pairs + runs + fifteens
 
-        print('TOTAL POINTS:', total_points)
-
+        # Remove cut card and return the hand to its original state
         self.remove_card(cut_card)
 
         return total_points
-
-
-
-
-
